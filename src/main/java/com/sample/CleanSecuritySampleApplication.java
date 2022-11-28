89542efc9c793997c1a53040b8dc4b666ce1f620
OzgurYatmaz <ozguryatmaz@gmail.com>
2022-11-29 00:32:30 +0300

    swagger2 is replaced by swagger3(open api)

--- src/main/java/com/sample/CleanSecuritySampleApplication.java
+++ src/main/java/com/sample/CleanSecuritySampleApplication.java
@@ -3,10 +3,12 @@ package com.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
-//@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Sample Spring Security App", version = "1.0", description = "A project made by spring boot 2.7.4"))
 public class CleanSecuritySampleApplication {
 
 	public static void main(String[] args) {
